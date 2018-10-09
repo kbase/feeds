81f764d1a5dfb926b7959c386a92fb5347cb213c
Bill Riehl <wjriehl@lbl.gov>
2018-10-09 11:52:14 -0700

    add basic notification and tests

--- feeds/activity/notification.py
+++ feeds/activity/notification.py
@@ -1,16 +1,25 @@
 from .base import BaseActivity
+import uuid
 
 class Notification(BaseActivity):
     def __init__(self, actor, note_type, note_object, target=None, content={}):
+        """
+        :param actor: user id of the actor (or kbase or global)
+        :param note_type: type of note, uses standard activity streams verbs
+        :param note_object: object of the note
+        :param target: target of the note
+        :param content: freeform content of the note
+        """
         self.actor = actor
         self.note_type = note_type
         self.object = note_object
         self.target = target
         self.content = content
+        self._id = uuid.uuid4()
 
     @property
-    def serialization_id(self):
-
+    def id(self):
+        return self._id
 
     def serialize(self):
         return
--- tests/test_notification.py
+++ tests/test_notification.py
@@ -1,1 +1,14 @@
 import pytest
+from feeds.activity.notification import Notification
+import uuid
+from .util import validate_uuid
+
+
+def test_basic_notification():
+    n = Notification('foo', 'bar', 'baz')
+    assert n.actor == 'foo'
+    assert n.note_type == 'bar'
+    assert n.object == 'baz'
+    assert n.content == {}
+    assert n.target == None
+    assert validate_uuid(n.id)
