81765fbc4d81cbcb68b41cb99d0eb5e71d6c42ff
Bill Riehl <wjriehl@lbl.gov>
2018-11-06 17:48:02 -0800

    add new files

---
+++ test-ui/src/feeds/controller.js
@@ -0,0 +1,64 @@
+import * as FeedsAPI from '../api/feeds';
+import Feed from './feed';
+
+export default class FeedController {
+    constructor() {
+        this.token = null;
+        this.notes = [];
+        this.element = document.createElement('div');
+        this.element.style.display = 'none';
+
+        this.header = document.createElement('h2');
+        this.header.innerText = "Notification Feed";
+        this.header.style.paddingTop = '20px';
+
+        this.globalFeed = new Feed('Global');
+        this.userFeed = new Feed();
+
+        this.element.appendChild(this.header);
+        this.element.appendChild(this.globalFeed.element);
+        this.element.appendChild(this.userFeed.element);
+    }
+
+    initialize(displayName, token) {
+        this.token = token;
+        this.displayName = displayName;
+        this.userFeed.setUserName(displayName + "'s");
+        this.removeFeed();
+        this.refreshFeed();
+    }
+
+    removeFeed() {
+        this.element.style.display = 'none';
+        this.globalFeed.remove();
+        this.userFeed.remove();
+    }
+
+    refreshFeed() {
+        FeedsAPI.getNotifications({}, this.token)
+            .then(feed => {
+                console.log(feed);
+                this.renderFeed(feed.data);
+            })
+            .catch(err => {
+                this.renderError(err);
+            });
+    }
+
+    renderFeed(feed) {
+        this.removeFeed();
+        this.globalFeed.updateFeed(feed.global);
+        this.userFeed.updateFeed(feed.user);
+        this.element.style.removeProperty('display');
+    }
+
+    renderError(err) {
+        console.log(err);
+        console.log(JSON.stringify(err));
+        this.element.innerHTML = `
+            <div class="alert alert-danger">
+                An error occurred while fetching your feed!
+            </div>
+        `;
+    }
+}
---
+++ test-ui/src/feeds/feed.js
@@ -0,0 +1,129 @@
+import Notification from './notification';
+
+export default class Feed {
+    constructor(userName, refreshFn) {
+        this.refreshFn = refreshFn;
+
+        this.element = document.createElement('div');
+        this.element.classList.add('card');
+        this.element.innerHTML = `
+            <div class="card-header">
+                <b><span id="user-feed-name">${userName}</span> notifications</b>
+                ${this.renderFilters()}
+            </div>
+            <div class="card-body"></div>
+        `;
+        this.ctrlState = {
+            seen: false,
+            descending: true,
+            levelFilter: null,
+            sourceFilter: null
+        };
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        let ctrls = this.element.querySelector('.card-header div.input-group');
+        // toggle eye
+        ctrls.querySelector('#seen-btn').onclick = () => {
+            let btnIcon = ctrls.querySelector('#seen-btn svg');
+            if (btnIcon.getAttribute('data-icon') === 'eye-slash') {
+                btnIcon.setAttribute('data-icon', 'eye');
+                this.ctrlState.seen = true;
+            }
+            else {
+                btnIcon.setAttribute('data-icon', 'eye-slash');
+                this.ctrlState.seen = false;
+            }
+            this.refresh();
+        }
+
+        // toggle order
+        ctrls.querySelector('#sort-btn').onclick = () => {
+            let btnIcon = ctrls.querySelector('#sort-btn svg');
+            if (btnIcon.getAttribute('data-icon') === 'sort-numeric-down') {
+                btnIcon.setAttribute('data-icon', 'sort-numeric-up');
+                this.ctrlState.descending = false;
+            }
+            else {
+                btnIcon.setAttribute('data-icon', 'sort-numeric-down');
+                this.ctrlState.descending = true;
+            }
+            this.refresh();
+        }
+
+        // level filter
+        ctrls.querySelector('#level-filter').onchange = (e) => {
+            console.log(e);
+            if (e.target.selectedIndex === 0) {
+                this.ctrlState.levelFilter = null;
+            }
+            else {
+                this.ctrlState.levelFilter = e.target.value;
+            }
+            this.refresh();
+        }
+
+        // source filter
+        ctrls.querySelector('#source-filter').onchange = (e) => {
+            if (e.target.selectedIndex === 0) {
+                this.ctrlState.sourceFilter = null;
+            }
+            else {
+                this.ctrlState.sourceFilter = e.target.value;
+            }
+            this.refresh();
+        }
+    }
+
+    refresh() {
+        // get filter info from controls
+        // run the refresh function
+        // update this feed with the results
+        console.log(this.ctrlState);
+    }
+
+    renderFilters() {
+        let levels = ['alert', 'warning', 'error', 'request'],
+            services = ['groups', 'workspace', 'jobs', 'narrative'],
+            filterHtml = `
+                <div class="input-group input-group-sm float-right" style="max-width: 350px">
+                    <button class="btn btn-outline-secondary" type="button" id="seen-btn">
+                        <i class="far fa-eye"></i>
+                    </button>
+                    <button class="btn btn-outline-secondary" type="button" id="sort-btn">
+                        <i class="fa fa-sort-numeric-down"></i>
+                    </button>
+                    <select class="custom-select" id="level-filter">
+                        <option selected>Filter Level</option>
+                        ${levels.map(level => `<option value="${level}">${level}</option>`)}
+                    </select>
+                    <select class="custom-select" id="source-filter">
+                        <option selected>Filter Source</option>
+                        ${services.map(service => `<option value="${service}">${service}</option>`)}
+                    </select>
+                </div>
+            `;
+
+        return filterHtml;
+    }
+
+    remove() {
+        this.element.querySelector('.card-body').innerHTML = '';
+    }
+
+    updateFeed(feed) {
+        this.remove();
+        let userFeed = this.element.querySelector('.card-body');
+        feed.forEach(note => {
+            let noteObj = new Notification(note);
+            userFeed.appendChild(noteObj.element);
+        });
+    }
+
+    setUserName(userName) {
+        this.userName = userName;
+        this.element.querySelector('#user-feed-name').innerHTML = userName;
+    }
+
+}
