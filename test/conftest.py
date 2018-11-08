4dd3644afaaacbc101108906aa6b21933f749aaa
Bill Riehl <wjriehl@lbl.gov>
2018-11-07 17:13:36 -0800

    more auth mocking

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@ import os
 import configparser
 import pytest
 import tempfile
+import json
 
 def pytest_sessionstart(session):
     os.environ['AUTH_TOKEN'] = 'foo'
@@ -40,3 +41,16 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
+@pytest.fixture
+def mock_valid_user(requests_mock):
+    def auth_valid_user(user_id, user_name):
+        cfg = test_config()
+        requests_mock.get('{}/api/V2/users?list={}'.format(cfg.get('feeds', 'auth-url'), user_id), text=json.dumps({user_id: user_name}))
+    return auth_valid_user
+
+@pytest.fixture
+def mock_invalid_user(requests_mock):
+    def auth_invalid_user(user_id):
+        cfg = test_config()
+        requests_mock.get('{}/api/V2/users?list={}'.format(cfg.get('feeds', 'auth-url'), user_id), text="{}")
+    return auth_invalid_user
