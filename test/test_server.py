106be33f415dea78494a0975b3c82e5d63ca3137
Bill Riehl <wjriehl@lbl.gov>
2018-11-14 14:46:48 -0800

    more tests!

--- test/test_server.py
+++ test/test_server.py
@@ -40,7 +40,6 @@ def test_server_post_paths_noauth(client, path):
 def test_root(client):
     response = client.get('/')
     data = json.loads(response.data)
-    print(data)
     assert 'servertime' in data
     assert 'service' in data and data['service'] == 'Notification Feeds Service'
     assert 'version' in data
@@ -113,3 +112,81 @@ def test_permissions_bad_token(client, mock_invalid_user_token):
     assert data['token'] == {'service': None, 'user': None, 'admin': False}
     assert 'permissions' in data
     assert data['permissions'] == {'GET': ['/api/V1/notifications/global'], 'POST': []}
+
+
+def test_server_illegal_param(client, mock_valid_service_token):
+    mock_valid_service_token('serv_user', 'serv_pw', 'SomeService')
+    response = client.post('/api/V1/notification', headers={'Authorization': 'token-'+str(uuid4())}, json=['bad', 'format'])
+    data = json.loads(response.data)
+    _validate_error(data, {'http_code': 400, 'http_status': 'Bad Request', 'message': 'Incorrect data format'})
+
+
+def test_server_invalid_token(client, mock_invalid_user_token):
+    user_id = 'a_user'
+    mock_invalid_user_token(user_id)
+    response = client.get('/api/V1/notifications', headers={'Authorization': 'token-'+str(uuid4())})
+    data = json.loads(response.data)
+    _validate_error(data, {'http_code': 401, 'http_status': 'Unauthorized', 'message': 'Invalid token'})
+
+
+def test_server_note_not_found(client, mock_valid_user_token):
+    user_id = 'a_user'
+    user_name = 'A User'
+    mock_valid_user_token(user_id, user_name)
+    response = client.get('/api/V1/notification/fake_note', headers={'Authorization': 'token-'+str(uuid4())})
+    data = json.loads(response.data)
+    _validate_error(data, {'http_code': 404, 'http_status': 'Not Found', 'message': 'Cannot find notification with id fake_note.'})
+
+
+def test_server_404(client):
+    path = '/not/a/real/path'
+    response = client.get(path)
+    data = json.loads(response.data)
+    _validate_error(data, {'http_code': 404, 'http_status': 'Not Found', 'message': 'Path {} not found.'.format(path)})
+
+
+def test_server_missing_params(client, mock_valid_service_token):
+    """
+    Calls the API, but here we're just checking that the proper error gets raised from the server.
+    The API tests will make sure the error content is correct.
+    """
+    mock_valid_service_token('serv_user', 'serv_pw', 'SomeService')
+    response = client.post('/api/V1/notification', headers={'Authorization': 'token-'+str(uuid4())}, json={'actor': 'nope'})
+    data = json.loads(response.data)
+    _validate_error(data, {
+        'http_code': 422,
+        'http_status': 'Unprocessable Entity',
+        'message': 'Missing parameters - verb, object, level, target, source'
+    })
+
+
+def test_server_405(client):
+    response = client.get('/api/V1/notification')
+    data = json.loads(response.data)
+    _validate_error(data, {'http_code': 405, 'http_status': 'Method Not Allowed', 'message': 'Method not allowed'})
+
+
+def test_server_auth_error(client, mock_auth_error):
+    response = client.get('/api/V1/notifications', headers={'Authorization': 'token-'+str(uuid4())})
+    _validate_error(response.data, {
+        "http_code": 500,
+        "http_status": "Internal Server Error",
+        "message": "Unable to fetch authentication information"
+    })
+
+
+def test_server_500(client, mock_valid_user_token):
+    pass
+
+
+def _validate_error(data, std_error):
+    """
+    Validates an error returned by the service.
+    :param data: dict - should have a single key "error"
+    :param std_error: dict - the standard to compare against. The keys and values here are expected to
+        all be in data['error']
+    """
+    assert 'error' in data
+    for k in std_error:
+        assert k in data['error']
+        assert data['error'][k] == std_error[k]
