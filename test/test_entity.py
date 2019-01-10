5be5c61135f4f8fc0d4eccc5cb494436d2edf2b6
Bill Riehl <wjriehl@lbl.gov>
2019-01-10 14:16:07 -0800

    add entity test stubs

---
+++ test/test_entity.py
@@ -0,0 +1,57 @@
+import pytest
+
+
+def test_entity_init_ok():
+    raise NotImplementedError()
+
+
+def test_entity_init_bad_type():
+    raise NotImplementedError()
+
+
+def test_entity_init_auto_validate():
+    raise NotImplementedError()
+
+
+def test_entity_validate_ok():
+    raise NotImplementedError()
+
+
+def test_entity_validate_fail():
+    raise NotImplementedError()
+
+
+def test_entity_to_dict():
+    raise NotImplementedError()
+
+
+def test_entity_from_dict():
+    raise NotImplementedError()
+
+
+def test_entity_from_dict_fail():
+    raise NotImplementedError()
+
+
+def test_entity_str():
+    raise NotImplementedError()
+
+
+def test_entity_from_str():
+    raise NotImplementedError()
+
+
+def test_entity_hash():
+    raise NotImplementedError()
+
+
+def test_entity_eq():
+    raise NotImplementedError()
+
+
+def test_entity_repr():
+    raise NotImplementedError()
+
+
+def test_entity_fetch_name():
+    raise NotImplementedError()
